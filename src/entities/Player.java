31b6fa56de632feb2e476d2962aaa6c87359b8a4
Jean <franciscojean053@gmail.com>
2024-04-28 15:52:06 -0300

    Criando a tela e os comandos

---
+++ src/entities/Player.java
@@ -0,0 +1,38 @@
+package entities;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import main.Game;
+
+public class Player {
+	
+	public boolean right, left;
+	public int x, y, width, height;
+	
+	public Player(int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.width = 40;
+		this.height = 10;
+	}
+	
+	public void tick() {
+		if(right) {
+			x++;
+		} else if(left){
+			x--;
+		}
+		
+		if(x + this.width > Game.WIDTH) {
+			x = Game.WIDTH - this.width;
+		} else if (x < 0) {
+			x = 0;
+		}
+	}
+	
+	public void render(Graphics g) {
+		g.setColor(Color.BLUE);
+		g.fillRect(x, y, this.width, this.height);
+	}
+}
--- src/main/Game.java
+++ src/main/Game.java
@@ -1,15 +1,28 @@
 package main;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
 
-public class Game extends Canvas implements Runnable{
+import entities.Player;
+
+public class Game extends Canvas implements Runnable, KeyListener{
 	private static final long serialVersionUID = 1L;
 	public static final int WIDTH = 240, HEIGHT = 120, SCALE = 3;
+	private Player player;
+	private BufferedImage layer = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+	
 	
 	public Game() {
+		this.player = new Player(100, HEIGHT - 10);
+		this.addKeyListener(this);
 		this.setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
 	}
 
@@ -22,11 +35,65 @@ public class Game extends Canvas implements Runnable{
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
+		new Thread(game).start();
+	}
+	
+	public void tick() {
+		player.tick();
+	}
+	
+	
+	public void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		Graphics g = layer.getGraphics();
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		player.render(g);
+		
+		g = bs.getDrawGraphics();
+		g.drawImage(layer, 0, 0, WIDTH*SCALE, HEIGHT*SCALE, null);
+		
+		bs.show();
 	}
 	
 	@Override
 	public void run() {
+		while(true) {
+			try {
+				tick();
+				render();
+				Thread.sleep(1000/60);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
 		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			player.right = true;
+		} else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+			player.left = true; 
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			player.right = false;
+		} else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+			player.left = false; 
+		}
 	}
 	
 	
